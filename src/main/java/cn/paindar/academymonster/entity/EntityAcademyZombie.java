e9ef9db90d7732f2fd5e02d5e7832b4655fdaa79
voidcl <ywsx3547@126.com>
2017-02-10 23:38:43 +0800

    right?

--- src/main/java/cn/paindar/academymonster/entity/EntityAcademyZombie.java
+++ src/main/java/cn/paindar/academymonster/entity/EntityAcademyZombie.java
@@ -1,11 +1,11 @@
 package cn.paindar.academymonster.entity;
 
-import cn.lambdalib.util.generic.RandUtils;
 import cn.paindar.academymonster.ability.AIElectronBomb;
 import cn.paindar.academymonster.ability.AIPenetrateTeleport;
 import cn.paindar.academymonster.ability.BaseAbility;
 import cn.paindar.academymonster.entity.ai.EntityAIElectronBomb;
 import cn.paindar.academymonster.entity.ai.EntityAIPenetrateTeleport;
+import cn.lambdalib.util.generic.RandUtils;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.IRangedAttackMob;
 import net.minecraft.entity.ai.*;
@@ -14,6 +14,8 @@ import net.minecraft.world.World;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.HashMap;
+import java.lang.reflect.Constructor;
 
 
 /**
@@ -23,19 +25,43 @@ public class EntityAcademyZombie extends EntityZombie implements IRangedAttackMo
 {
     private final EntityAIBreakDoor aIBreakDoor = new EntityAIBreakDoor(this);
     private boolean canBreakDoor=false;
-    public List<BaseAbility> abilityList=new ArrayList<>();
-
+    private HashMap<BaseAbility,String> AbilityMap=new HashMap<BaseAbility,String>();
     public EntityAcademyZombie(World world) {
         super(world);
-        //abilityList.add(new AIPenetrateTeleport(this,1- RandUtils.rangef(0,1)*RandUtils.rangef(0,1)));
-        //abilityList.add(new AIElectronBomb(this,1- RandUtils.rangef(0,1)*RandUtils.rangef(0,1)));
-        for(BaseAbility skill:abilityList)
+        AbilityMap.put(new AIPenetrateTeleport(this,1-RandUtils.rangef(0,1)*RandUtils.rangef(0,1)),"4～EntityAIPenetrateTeleport");
+        AbilityMap.put(new AIElectronBomb(this,1),"5～EntityAIElectronBomb");//按照类似格式添加进map   4～指的是优先级4
+        //abilityList.add(new AIPenetrateTeleport(this,1-RandUtils.rangef(0,1)*RandUtils.rangef(0,1)));
+        //abilityList.add(new AIElectronBomb(this,1));
+        BaseAbility skill=null;
+        EntityAIBase BaseAI;
+        String AbilityType=new String(AbilityMap.get(skill));
+        if (AbilityType.equals(null))
         {
-            if(skill instanceof AIElectronBomb)
-                this.tasks.addTask(4,new EntityAIElectronBomb(this,(AIElectronBomb)skill));
-            else if (skill instanceof AIPenetrateTeleport)
-                this.tasks.addTask(5,new EntityAIPenetrateTeleport(this,(AIPenetrateTeleport)skill));
+
         }
+        else
+        {
+            String mid[]=new String[2];
+            mid=AbilityType.split("~");
+            try
+            {
+                Class temp1=Class.forName("cn.painder.academymonster.ability."+mid[1]);
+                Constructor constructor1=temp1.getConstructor(Object.class,Double.class);
+                skill=(BaseAbility)constructor1.newInstance(this,1-RandUtils.rangef(0,1)*RandUtils.rangef(0,1));//动态生成技能对象
+
+                Class temp2=Class.forName("cn.painder.academymonster.entity.ai.Entity"+mid[1]);
+                Constructor constructor2=temp2.getConstructor(Object.class,Object.class);
+                BaseAI=(EntityAIBase)constructor2.newInstance(this,skill);//动态生成怪物AI
+
+                this.tasks.addTask(Integer.valueOf(mid[0]),BaseAI);//加入怪物AI至任务
+            }catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+
+
+
 
     }
 
