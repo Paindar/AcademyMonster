247033cd50e19d1e06f678f219863ff9fae5e505
Paindar <1944798085@qq.com>
2017-02-11 23:11:27 +0800

    【生物生成】修复生成时可能产生的崩溃，并手动分配了技能

--- src/main/java/cn/paindar/academymonster/entity/EntityAcademyEnderman.java
+++ src/main/java/cn/paindar/academymonster/entity/EntityAcademyEnderman.java
@@ -1,9 +1,11 @@
 package cn.paindar.academymonster.entity;
 
 import cn.lambdalib.util.generic.RandUtils;
+import cn.paindar.academymonster.ability.AIBodyIntensify;
 import cn.paindar.academymonster.ability.AIElectronBomb;
 import cn.paindar.academymonster.ability.AIFleshRipping;
 import cn.paindar.academymonster.ability.BaseAbility;
+import cn.paindar.academymonster.entity.ai.EntityAIBodyIntensify;
 import cn.paindar.academymonster.entity.ai.EntityAIElectronBomb;
 import cn.paindar.academymonster.entity.ai.EntityAIFleshRipping;
 import javafx.util.Pair;
@@ -24,19 +26,20 @@ import java.util.List;
  */
 public class EntityAcademyEnderman extends EntityEnderman
 {
+
     private static HashMap<Class<? extends EntityAIBase>,Integer> aiLevel=new HashMap<>();
     private static List<Pair<Class<? extends BaseAbility>,Class<? extends EntityAIBase>>> validSkillList=new ArrayList<>();
     static
     {
-//        validSkillList.add(new Pair<>(AIElectronBomb.class, EntityAIElectronBomb.class));
-//        aiLevel.put(EntityAIElectronBomb.class,5);
+        validSkillList.add(new Pair<>(AIBodyIntensify.class, EntityAIBodyIntensify.class));
+        aiLevel.put(EntityAIBodyIntensify.class,5);
+        validSkillList.add(new Pair<>(AIElectronBomb.class, EntityAIElectronBomb.class));
+        aiLevel.put(EntityAIElectronBomb.class,5);
         validSkillList.add(new Pair<>(AIFleshRipping.class, EntityAIFleshRipping.class));
         aiLevel.put(EntityAIFleshRipping.class,5);
     }
 
-
-
-    private static float factor=0.5f;
+    private float factor = 0.3f;
     public EntityAcademyEnderman(World world) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException
     {
         super(world);
@@ -52,16 +55,17 @@ public class EntityAcademyEnderman extends EntityEnderman
                 break;
             else
             {
-                constructor=validSkillList.get(level).getKey().getConstructor(EntityLivingBase.class,float.class);
+                Pair<Class<? extends BaseAbility>,Class<? extends EntityAIBase>> elem=validSkillList.get(level);
+                constructor=elem.getKey().getConstructor(EntityLivingBase.class,float.class);
                 skill=(BaseAbility)constructor.newInstance(this,1-RandUtils.rangef(0,1)*RandUtils.rangef(0,1));//动态生成技能对象
-                Class aclass=validSkillList.get(level).getValue();
+                Class aclass=elem.getValue();
                 Constructor[] tempconstructor=aclass.getDeclaredConstructors();
                 Class[] parameterTypes=tempconstructor[0].getParameterTypes();
-                constructor=validSkillList.get(level).getValue().getConstructor(parameterTypes[0],parameterTypes[1]);
+                constructor=elem.getValue().getConstructor(parameterTypes[0],parameterTypes[1]);
+                //AcademyMonster.log.info("param1="+parameterTypes[0]+" param2 "+parameterTypes[1]+" skill= "+skill);
                 baseAI=(EntityAIBase)constructor.newInstance(this,skill);//动态生成怪物AI
                 this.tasks.addTask(aiLevel.get(baseAI.getClass()),baseAI);//加入怪物AI至任务
                 prob*=factor;
-
             }
             level++;
         }
--- src/main/java/cn/paindar/academymonster/entity/EntityAcademySkeleton.java
+++ src/main/java/cn/paindar/academymonster/entity/EntityAcademySkeleton.java
@@ -1,14 +1,22 @@
 package cn.paindar.academymonster.entity;
 
-import cn.paindar.academymonster.ability.AIElectronBomb;
-import cn.paindar.academymonster.ability.AIPenetrateTeleport;
-import cn.paindar.academymonster.ability.BaseAbility;
+import cn.lambdalib.util.generic.RandUtils;
+import cn.paindar.academymonster.ability.*;
+import cn.paindar.academymonster.entity.ai.EntityAIBodyIntensify;
 import cn.paindar.academymonster.entity.ai.EntityAIElectronBomb;
+import cn.paindar.academymonster.entity.ai.EntityAIFleshRipping;
 import cn.paindar.academymonster.entity.ai.EntityAIPenetrateTeleport;
+import javafx.util.Pair;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.ai.EntityAIBase;
 import net.minecraft.entity.monster.EntitySkeleton;
 import net.minecraft.world.World;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -16,17 +24,45 @@ import java.util.List;
  */
 public class EntityAcademySkeleton extends EntitySkeleton
 {
+    private static HashMap<Class<? extends EntityAIBase>,Integer> aiLevel=new HashMap<>();
+    private static List<Pair<Class<? extends BaseAbility>,Class<? extends EntityAIBase>>> validSkillList=new ArrayList<>();
+    static
+    {
+        validSkillList.add(new Pair<>(AIBodyIntensify.class, EntityAIBodyIntensify.class));
+        aiLevel.put(EntityAIBodyIntensify.class,5);
+        validSkillList.add(new Pair<>(AIElectronBomb.class, EntityAIElectronBomb.class));
+        aiLevel.put(EntityAIElectronBomb.class,5);
+    }
 
-    public List<BaseAbility> abilityList=new ArrayList<>();
-    public EntityAcademySkeleton(World world) {
+    private float factor = 0.3f;
+    public EntityAcademySkeleton(World world) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException
+    {
         super(world);
-        abilityList.add(new AIElectronBomb(this,1));
-        for(BaseAbility skill:abilityList)
+        Collections.shuffle(validSkillList);
+        float prob=1f;
+        int level=0;
+        BaseAbility skill=null;
+        EntityAIBase baseAI=null;
+        Constructor constructor=null;
+        while(RandUtils.nextFloat()<=prob)
         {
-            if(skill instanceof AIElectronBomb)
-                this.tasks.addTask(4,new EntityAIElectronBomb(this,(AIElectronBomb)skill));
-            else if (skill instanceof AIPenetrateTeleport)
-                this.tasks.addTask(5,new EntityAIPenetrateTeleport(this,(AIPenetrateTeleport)skill));
+            if(level>=validSkillList.size())
+                break;
+            else
+            {
+                Pair<Class<? extends BaseAbility>,Class<? extends EntityAIBase>> elem=validSkillList.get(level);
+                constructor=elem.getKey().getConstructor(EntityLivingBase.class,float.class);
+                skill=(BaseAbility)constructor.newInstance(this,1-RandUtils.rangef(0,1)*RandUtils.rangef(0,1));//动态生成技能对象
+                Class aclass=elem.getValue();
+                Constructor[] tempconstructor=aclass.getDeclaredConstructors();
+                Class[] parameterTypes=tempconstructor[0].getParameterTypes();
+                constructor=elem.getValue().getConstructor(parameterTypes[0],parameterTypes[1]);
+                //AcademyMonster.log.info("param1="+parameterTypes[0]+" param2 "+parameterTypes[1]+" skill= "+skill);
+                baseAI=(EntityAIBase)constructor.newInstance(this,skill);//动态生成怪物AI
+                this.tasks.addTask(aiLevel.get(baseAI.getClass()),baseAI);//加入怪物AI至任务
+                prob*=factor;
+            }
+            level++;
         }
     }
 
