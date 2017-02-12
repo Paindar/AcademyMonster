48c1639ffeeeec01fe7935e172fc48dddbdb4dbc
Paindar <1944798085@qq.com>
2017-02-13 00:00:43 +0800

    【定向冲力】添加新技能

---
+++ src/main/java/cn/paindar/academymonster/ability/AIDirectedShock.java
@@ -0,0 +1,73 @@
+package cn.paindar.academymonster.ability;
+
+import cn.academy.vanilla.vecmanip.skill.DirectedShock;
+import cn.lambdalib.util.mc.EntitySelectors;
+import cn.lambdalib.util.mc.Raytrace;
+import cn.lambdalib.util.mc.WorldUtils;
+import cn.paindar.academymonster.network.NetworkManager;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.util.MovingObjectPosition;
+import net.minecraft.util.Vec3;
+
+import java.util.List;
+
+import static cn.lambdalib.util.generic.MathUtils.lerpf;
+
+/**
+ * Created by Paindar on 2017/2/12.
+ */
+public class AIDirectedShock extends BaseAbility
+{
+    private final float maxDistance=3;
+    private float damage;
+    public AIDirectedShock(EntityLivingBase speller, float abilityExp)
+    {
+        super(speller, (int)lerpf(60, 20, abilityExp), abilityExp);
+        damage=lerpf(7, 15,abilityExp);
+    }
+
+    private void knockback(Entity targ)
+    {
+        if (getSkillExp() >= 0.25f)
+        {
+            Vec3 slope=speller.getLookVec();
+            targ.setPosition(targ.posX, targ.posY + 0.1, targ.posZ);
+            targ.setVelocity(slope.xCoord*3f,slope.yCoord*3f,slope.zCoord*3f);
+        }
+    }
+
+
+    public void spell()
+    {
+        MovingObjectPosition result = Raytrace.traceLiving(speller, 3, EntitySelectors.living());
+        if(result!=null && result.typeOfHit== MovingObjectPosition.MovingObjectType.ENTITY)
+        {
+
+            if(result.entityHit instanceof EntityLivingBase)
+            {
+                EntityLivingBase entity=(EntityLivingBase)result.entityHit;
+                attack((EntityLivingBase) result.entityHit, damage);
+                knockback(entity);
+                List<Entity> list= WorldUtils.getEntities(speller, 25, EntitySelectors.player());
+                for(Entity e:list)
+                {
+                    NetworkManager.sendSoundTo("vecmanip.directed_shock",(EntityPlayerMP)e);
+                }
+            }
+        }
+        super.spell();
+    }
+
+    public float getMaxDistance()
+    {
+        return maxDistance;
+    }
+
+    @Override
+    public String getSkillName()
+    {
+        return DirectedShock.getDisplayName();
+    }
+}
