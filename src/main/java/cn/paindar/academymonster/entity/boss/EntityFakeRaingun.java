3b8ad2011ece8eed1fbe94e7d8972cb482a7b815
Paindar <1944798085@qq.com>
2017-03-18 14:13:12 +0800

    【伪电磁炮】修复电磁炮跑的太快的问题

--- src/main/java/cn/paindar/academymonster/entity/boss/EntityFakeRaingun.java
+++ src/main/java/cn/paindar/academymonster/entity/boss/EntityFakeRaingun.java
@@ -4,8 +4,11 @@ import cn.academy.ability.ModuleAbility;
 import cn.academy.vanilla.ModuleVanilla;
 import cn.lambdalib.util.generic.RandUtils;
 import cn.paindar.academymonster.ability.*;
+import cn.paindar.academymonster.core.AcademyMonster;
+import cn.paindar.academymonster.entity.SkillExtendedEntityProperties;
 import cn.paindar.academymonster.entity.ai.EntityAIFakeRailgunAttack;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.*;
 import net.minecraft.entity.boss.IBossDisplayData;
@@ -17,6 +20,7 @@ import net.minecraft.item.Item;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.world.World;
 
+import java.lang.reflect.Constructor;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -32,7 +36,7 @@ public class EntityFakeRaingun extends EntityMob implements IBossDisplayData
         this.setHealth(this.getMaxHealth());
         this.getNavigator().setCanSwim(true);
         this.tasks.addTask(0, new EntityAISwimming(this));
-        this.tasks.addTask(2, new EntityAIAttackOnCollide(this, EntityPlayer.class, 8.0D, false));
+        this.tasks.addTask(2, new EntityAIAttackOnCollide(this, EntityPlayer.class,  0.6D, false));
         this.tasks.addTask(4, new EntityAIFakeRailgunAttack(this));
         this.tasks.addTask(5, new EntityAIWander(this, 1.0D));
         this.tasks.addTask(6, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
@@ -46,28 +50,64 @@ public class EntityFakeRaingun extends EntityMob implements IBossDisplayData
         skillList.add(new AIRailgun(this,0.5f+ RandUtils.rangef(0,0.5f)));
         skillList.add(new AIThunderBolt(this,RandUtils.rangef(0,1f)));
         skillList.add(new AIThunderClap(this, RandUtils.rangef(0,1f)));
+
+        StringBuilder info=new StringBuilder();
+        for(BaseSkill skill:skillList)
+        {
+            float exp=skill.getSkillExp();
+            info.append(skill.getUnlocalizedSkillName()+"~"+exp+"-");
+        }
+        SkillExtendedEntityProperties properties= SkillExtendedEntityProperties.get(this);
+        properties.setSkillData(info.toString());
     }
 
     public void writeEntityToNBT(NBTTagCompound nbt)
     {
         super.writeEntityToNBT(nbt);
-        nbt.setFloat("ArcGen",skillList.get(0).getSkillExp());
-        nbt.setFloat("BodyIntensify",skillList.get(1).getSkillExp());
-        nbt.setFloat("Railgun",skillList.get(2).getSkillExp());
-        nbt.setFloat("ThunderBolt",skillList.get(3).getSkillExp());
-        nbt.setFloat("ThunderClap",skillList.get(4).getSkillExp());
+        StringBuffer sbuffer=new StringBuffer();
+        for(BaseSkill skill:skillList)
+        {
+            String name=skill.getClass().getName();
+            sbuffer.append(name+"-");
+            nbt.setFloat(name,skill.getSkillExp());
+        }
+        nbt.setString("allSkill",sbuffer.toString());
     }
-
-
     public void readEntityFromNBT(NBTTagCompound nbt)
     {
-        super.readEntityFromNBT(nbt);
         skillList.clear();
-        skillList.add(new AIArcGen(this,nbt.getFloat("ArcGen")));
-        skillList.add(new AIBodyIntensify(this,nbt.getFloat("BodyIntensify")));
-        skillList.add(new AIThunderBolt(this,nbt.getFloat("ThunderBolt")));
-        skillList.add(new AIRailgun(this,nbt.getFloat("Railgun")));
-        skillList.add(new AIThunderClap(this,nbt.getFloat("ThunderClap")));
+        String[] list=nbt.getString("allSkill").split("-");
+        StringBuilder info=new StringBuilder();
+        for(String item:list)
+        {
+            if(item.isEmpty())
+                continue;
+            try
+            {
+                Class<?> klass=Class.forName(item);
+                Constructor constructor;
+                BaseSkill skill;
+                float exp=nbt.getFloat(item);
+                try
+                {
+                    constructor = klass.getConstructor(EntityLivingBase.class, float.class);
+                    skill = (BaseSkill) constructor.newInstance(this, exp);
+                    skillList.add(skill);
+                    info.append(skill.getUnlocalizedSkillName()+"~"+exp+"-");
+                }
+                catch(Exception e)
+                {
+                    e.printStackTrace();
+                }
+            } catch (ClassNotFoundException e)
+            {
+                e.printStackTrace();
+            }
+
+        }
+        super.readEntityFromNBT(nbt);
+        SkillExtendedEntityProperties properties= SkillExtendedEntityProperties.get(this);
+        properties.setSkillData(info.toString());
     }
 
     public int getTotalArmorValue()
@@ -85,7 +125,7 @@ public class EntityFakeRaingun extends EntityMob implements IBossDisplayData
         this.dropItem(Item.getItemFromBlock(Blocks.iron_block), RandUtils.nextInt(2));
         this.dropItem(ModuleVanilla.coin,RandUtils.nextInt(5)+3);
         if(RandUtils.nextFloat()<=1)
-            this.dropItem(ModuleAbility.inductionFactor.create(ModuleVanilla.electromaster).getItem(),1);
+            this.entityDropItem(ModuleAbility.inductionFactor.create(ModuleVanilla.electromaster),1);
 
     }
 
