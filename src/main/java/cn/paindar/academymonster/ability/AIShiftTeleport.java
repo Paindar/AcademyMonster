3d8a100921fdf1598ab31b3636ffd680a06c953e
voidcl <ywsx3547@126.com>
2017-03-19 19:49:42 +0800

    【转移传送】新增转移传送
    将怪物脚下方块转移至目标头部附近（或头部位置），同样未在AcadymyMonster中注册（这个技能只是不想改类名字写的。。。。。qwq

---
+++ src/main/java/cn/paindar/academymonster/ability/AIShiftTeleport.java
@@ -0,0 +1,64 @@
+package cn.paindar.academymonster.ability;
+
+import cn.lambdalib.util.generic.RandUtils;
+import cn.lambdalib.util.mc.EntitySelectors;
+import cn.lambdalib.util.mc.Raytrace;
+import cn.lambdalib.util.mc.WorldUtils;
+import cn.paindar.academymonster.network.NetworkManager;
+import net.minecraft.block.Block;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.util.MovingObjectPosition;
+import net.minecraft.world.World;
+
+import java.util.List;
+
+import static cn.lambdalib.util.generic.MathUtils.lerpf;
+/**
+ * Created by voidcl on 2017/3/19.
+ */
+public class AIShiftTeleport extends BaseSkill{
+    private float exp;
+    private float maxdistance;
+    public AIShiftTeleport(EntityLivingBase speller,float exp)
+    {
+        super(speller,(int)lerpf(40,20,exp),exp,"teleporter.shift_tp");
+        this.exp=exp;
+        maxdistance=lerpf(3,10,getSkillExp());
+    }
+
+
+    public float getMaxdistance()
+    {
+        return maxdistance;
+    }
+    public void spell()
+    {
+        MovingObjectPosition result=Raytrace.traceLiving(speller,2,EntitySelectors.living());
+        EntityLivingBase target=null;
+        if(result!=null&&result.typeOfHit==MovingObjectPosition.MovingObjectType.ENTITY)
+        {
+            target=(EntityLivingBase)result.entityHit;
+        }
+
+        if(isSkillInCooldown()||target==null)
+            return;
+        int ix=(int)target.posX+RandUtils.nextInt(1)-RandUtils.nextInt(1);int iy=(int)target.posY+1;int iz=(int)target.posZ;
+        World world=speller.worldObj;
+        Block b=world.getBlock((int)speller.posX,(int)speller.posY-1,(int)speller.posZ);
+        if(b.isCollidable()&&!b.getMaterial().isReplaceable()) {
+            world.setBlock(ix, iy, iz, b);
+            world.setBlockToAir((int) speller.posX, (int) speller.posY - 1, (int) speller.posZ);
+            List<Entity> list = WorldUtils.getEntities(speller, 25, EntitySelectors.player());
+            for (Entity e : list) {
+                NetworkManager.sendSoundTo("tp.tp", target, .5f, (EntityPlayerMP) e);
+            }
+
+        }else
+        {
+            return;
+        }
+        super.spell();
+    }
+}
