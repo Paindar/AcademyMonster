ee80ae7509a067ba64da24e0d21408db255815fa
voidcl <ywsx3547@126.com>
2017-03-21 10:31:41 +0800

    【定位传送】新增定位转送
    攻击方与被击方同时移动，如果上方10格没有障碍物，瞬间两者移动至上方10格处，可叠加。如果有障碍物，转而移向地下2格，不可叠加。未在AM中注册

---
+++ src/main/java/cn/paindar/academymonster/ability/AILocationTeleport.java
@@ -0,0 +1,81 @@
+package cn.paindar.academymonster.ability;
+
+import cn.lambdalib.util.generic.RandUtils;
+import cn.lambdalib.util.mc.EntitySelectors;
+import cn.lambdalib.util.mc.WorldUtils;
+import cn.paindar.academymonster.network.NetworkManager;
+import net.minecraft.block.Block;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.world.World;
+
+import java.util.List;
+
+import static cn.lambdalib.util.generic.MathUtils.lerpf;
+
+/**
+ * Created by voidcl on 2017/3/20.
+ */
+public class AILocationTeleport extends BaseSkill {
+
+    private int DropHeight=10;
+    private int InGround=2;
+
+    public AILocationTeleport(EntityLivingBase speller,float exp)
+    {
+        super(speller,(int)lerpf(60,20,exp),exp,"teleporter.location_teleport");
+    }
+
+    public boolean hasPlace(double x,double y,double z)
+    {
+        World world=speller.worldObj;
+        Block b1=world.getBlock((int)x,(int)y,(int)z);
+        Block b2=world.getBlock((int)x,(int)y+1,(int)z);
+        return !b1.canCollideCheck(world.getBlockMetadata((int)x, (int)y, (int)z), false) && !b2.canCollideCheck(world.getBlockMetadata((int)x, (int)y+1, (int)z), false);
+
+    }
+
+    public boolean SkyOrGround(EntityLivingBase target)
+    {
+        if(hasPlace(target.posX,target.posY+10,target.posZ))
+        {
+            return true;
+        }else
+        {
+            return false;
+        }
+    }
+
+    public void spell(EntityLivingBase target)
+    {
+        System.out.println("dddddd");
+        int rand=RandUtils.nextInt(1)-RandUtils.nextInt(1);
+        if(target!=null&&!isSkillInCooldown())
+        {
+            if(SkyOrGround(target))
+            {
+                speller.setPositionAndUpdate(target.posX,target.posY+DropHeight,target.posZ);
+                target.setPositionAndUpdate(target.posX+rand,target.posY+DropHeight,target.posZ+rand);
+            }else
+            {
+                speller.setPositionAndUpdate(target.posX,target.posY-InGround,target.posZ);
+                target.setPositionAndUpdate(target.posX+rand,target.posY-InGround,target.posZ+rand);
+            }
+            if(!speller.worldObj.isRemote)
+            {
+                List<Entity> list= WorldUtils.getEntities(speller, 25, EntitySelectors.player());
+                for(Entity e:list)
+                {
+
+                    NetworkManager.sendSoundTo("tp.tp",target,.5f,(EntityPlayerMP)e);
+                }
+            }
+        }else
+        {
+            return ;
+        }
+        super.spell();
+    }
+
+}
