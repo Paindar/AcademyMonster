178ebd84d7ca5eaa58db88664b0ef1e5d808a8ad
Paindar <1944798085@qq.com>
2017-05-29 01:35:39 +0800

    [技能管理]删除原先的AI生成

--- src/main/java/cn/paindar/academymonster/core/SkillManager.java
+++ src/main/java/cn/paindar/academymonster/core/SkillManager.java
@@ -25,7 +25,6 @@ public class SkillManager
     {
         Class<? extends BaseSkill> klass;
         float prob;
-        Class<? extends EntityAIBase> aiType;
         int prop;
         int lvl;
         String name;
@@ -34,66 +33,65 @@ public class SkillManager
         @Override
         public String toString()
         {
-            StringBuilder builder=new StringBuilder();
-            builder.append(klass).append(" prob=").append(prob).append(" AIType= ").append(aiType)
-                    .append(" prop= ").append(prop).append(" level = ").append(lvl).append(" skill name = ").append(name)
-                    .append(" catalog type = ").append(type).append(" at ").append(super.toString());
-            return builder.toString();
+            return String.valueOf(klass) + " prob=" + prob +
+                    " prop= " + prop + " level = " + lvl + " skill name = " + name +
+                    " catalog type = " + type + " at " + super.toString();
         }
     }
     public enum Catalog{vector,meltdown,electro,teleport}
     public static SkillManager instance=new SkillManager();
     private static List<SkillInfo> list=new ArrayList<>();
+    public Map<Integer,BaseSkill> effectMgr=new HashMap<>();
 
     private SkillManager(){}
 
-    public void addSkillAI(BaseSkill skill,EntityLiving entity)
-    {
-        if(entity.worldObj.isRemote)
-            return;
-        Constructor constructor=null;
-        Class<? extends EntityAIBase> aClass;
-        Constructor[] tempConstructor;
-        Class[] parameterTypes=new Class[2];
-        SkillInfo info=null;
-        for(int i=0;i<list.size();i++)
-        {
-            info=list.get(i);
-            if(info.name.equals(skill.getUnlocalizedSkillName()))
-                break;
-        }
-        try
-        {
-            aClass = info.aiType;
-            if(aClass!=null)
-            {
-                tempConstructor = aClass.getDeclaredConstructors();
-                parameterTypes = tempConstructor[0].getParameterTypes();
-                constructor = aClass.getConstructor(parameterTypes[0], parameterTypes[1]);
-            }
-            else
-                return;
-        }
-        catch(Exception e)
-        {
-            AcademyMonster.log.error("No such constructor: (EntityLivingBase.class, float.class)");
-            e.printStackTrace();
-        }
-        if(constructor==null)
-            return;
-        EntityAIBase baseAI=null;
-        try
-        {
-            baseAI = (EntityAIBase) constructor.newInstance(entity, skill);
-        }
-        catch (Exception e)
-        {
-            AcademyMonster.log.error("param1: " + parameterTypes[0] + " , param2:" + parameterTypes[1]);
-            AcademyMonster.log.error("Argument: " + entity + " , " + skill);
-            e.printStackTrace();
-        }
-        entity.tasks.addTask(info.prop, baseAI);
-    }
+//    public void addSkillAI(BaseSkill skill,EntityLiving entity)
+//    {
+//        if(entity.worldObj.isRemote)
+//            return;
+//        Constructor constructor=null;
+//        Class<? extends EntityAIBase> aClass;
+//        Constructor[] tempConstructor;
+//        Class[] parameterTypes=new Class[2];
+//        SkillInfo info=null;
+//        for(int i=0;i<list.size();i++)
+//        {
+//            info=list.get(i);
+//            if(info.name.equals(skill.getUnlocalizedSkillName()))
+//                break;
+//        }
+//        try
+//        {
+//            aClass = info.aiType;
+//            if(aClass!=null)
+//            {
+//                tempConstructor = aClass.getDeclaredConstructors();
+//                parameterTypes = tempConstructor[0].getParameterTypes();
+//                constructor = aClass.getConstructor(parameterTypes[0], parameterTypes[1]);
+//            }
+//            else
+//                return;
+//        }
+//        catch(Exception e)
+//        {
+//            AcademyMonster.log.error("No such constructor: (EntityLivingBase.class, float.class)");
+//            e.printStackTrace();
+//        }
+//        if(constructor==null)
+//            return;
+//        EntityAIBase baseAI=null;
+//        try
+//        {
+//            baseAI = (EntityAIBase) constructor.newInstance(entity, skill);
+//        }
+//        catch (Exception e)
+//        {
+//            AcademyMonster.log.error("param1: " + parameterTypes[0] + " , param2:" + parameterTypes[1]);
+//            AcademyMonster.log.error("Argument: " + entity + " , " + skill);
+//            e.printStackTrace();
+//        }
+//        entity.tasks.addTask(info.prop, baseAI);
+//    }
     public BaseSkill createSkillInstance(String skillName, EntityLivingBase speller, float exp)
     {
         BaseSkill skill;
@@ -123,7 +121,7 @@ public class SkillManager
         return null;
     }
 
-    private void registerSkill(Class<? extends BaseSkill> skill,float defaultProb,Class<? extends EntityAIBase> aiClass,int aiLevel,int skillLevel,Catalog type)
+    private void registerSkill(Class<? extends BaseSkill> skill,float defaultProb,int aiLevel,int skillLevel,Catalog type)
     {
         float prob=(float) AMConfig.getDouble("am.skill."+skill.getSimpleName().substring(2)+".prob",defaultProb);
         if (prob<=1e-6)
@@ -131,7 +129,6 @@ public class SkillManager
         SkillInfo info=new SkillInfo();
         info.klass=skill;
         info.prob=defaultProb;
-        info.aiType=aiClass;
         info.prop=aiLevel;
         info.lvl=skillLevel;
         info.type=type;
@@ -153,26 +150,26 @@ public class SkillManager
     }
     void initSkill()
     {
-        registerSkill(AIArcGen.class,1,EntityAIArcGen.class,5,1,Catalog.electro);
-        registerSkill(AIBodyIntensify.class, 1,EntityAIBodyIntensify.class,5,2,Catalog.electro);
-        registerSkill(AIBloodRetrograde.class,0.7f,EntityAIBloodRetrograde.class,5,3,Catalog.vector);
-        registerSkill(AIDirectedShock.class, 2,EntityAIDirectedShock.class,5,1,Catalog.vector);
-        registerSkill(AIElectronBomb.class, 1,EntityAIElectronBomb.class,5,1,Catalog.meltdown);
-        registerSkill(AIElectronMissile.class,0.1f,EntityAIElectronMissile.class,5,5,Catalog.meltdown);
-        registerSkill(AIFlashing.class,0.2f,EntityAIFlashing.class,5,5,Catalog.teleport);
-        registerSkill(AIFleshRipping.class, 1,EntityAIFleshRipping.class,5,3,Catalog.teleport);
-        registerSkill(AIGroundShock.class, 1f,EntityAIGroundShock.class,5,2,Catalog.vector);
-        registerSkill(AILightShield.class,1f,EntityAILightShield.class,5,2,Catalog.meltdown);
-        registerSkill(AILocationTeleport.class,0.7f,EntityAILocationTeleport.class,5,3,Catalog.teleport);
-        registerSkill(AIMeltdowner.class,0.1f,EntityAIMeltdowner.class,5,4,Catalog.meltdown);
-        registerSkill(AIMineRay.class,0.7f,EntityAIMineRay.class,5,3,Catalog.meltdown);
-        registerSkill(AIPenetrateTeleport.class, 1,EntityAIPenetrateTeleport.class,4,2,Catalog.teleport);
-        registerSkill(AIRailgun.class, 0.3f,EntityAIRailgun.class,5,4,Catalog.electro);
-        registerSkill(AIThunderBolt.class,0.7f,EntityAIThunderBlot.class,5,4,Catalog.electro);
-        registerSkill(AIThunderClap.class,0.4f,EntityAIThunderClap.class,5,5,Catalog.electro);
-        registerSkill(AIVecReflect.class, 0.3f,null,5,3,Catalog.vector);
-        registerSkill(AIScatterBomb.class,0.5f,EntityAIScatterBomb.class,5,2,Catalog.meltdown);
-        registerSkill(AIShiftTeleport.class,0.7f,EntityAIShiftTeleport.class,5,3,Catalog.teleport);
+        registerSkill(AIArcGen.class,1,5,1,Catalog.electro);
+        registerSkill(AIBodyIntensify.class, 1,5,2,Catalog.electro);
+        registerSkill(AIBloodRetrograde.class,0.7f,5,3,Catalog.vector);
+        registerSkill(AIDirectedShock.class, 2,5,1,Catalog.vector);
+        registerSkill(AIElectronBomb.class, 1,5,1,Catalog.meltdown);
+        registerSkill(AIElectronMissile.class,0.1f,5,5,Catalog.meltdown);
+        //registerSkill(AIFlashing.class,0.2f,5,5,Catalog.teleport);
+        registerSkill(AIFleshRipping.class, 1,5,3,Catalog.teleport);
+        registerSkill(AIGroundShock.class, 1f,5,2,Catalog.vector);
+        //registerSkill(AILightShield.class,1f,5,2,Catalog.meltdown);
+        registerSkill(AILocationTeleport.class,0.7f,5,3,Catalog.teleport);
+        registerSkill(AIMeltdowner.class,0.1f,5,4,Catalog.meltdown);
+        //registerSkill(AIMineRay.class,0.7f,5,3,Catalog.meltdown);
+        registerSkill(AIPenetrateTeleport.class, 1,4,2,Catalog.teleport);
+        registerSkill(AIRailgun.class, 0.3f,5,4,Catalog.electro);
+        registerSkill(AIThunderBolt.class,0.7f,5,4,Catalog.electro);
+        registerSkill(AIThunderClap.class,0.4f,5,5,Catalog.electro);
+        registerSkill(AIVecReflect.class, 0.3f,5,3,Catalog.vector);
+        registerSkill(AIScatterBomb.class,0.5f,5,2,Catalog.meltdown);
+        //registerSkill(AIShiftTeleport.class,0.7f,5,3,Catalog.teleport);
         list.sort((a,b)->(a.type!=b.type)?(a.type.ordinal()<b.type.ordinal()?-1:1):(a.lvl!=b.lvl?(a.lvl<b.lvl?-1:1):0));
     }
 
@@ -222,7 +219,6 @@ public class SkillManager
 
             int level = 1, last = 0, mark = 0;
             List<SkillInfo> filtList = new ArrayList<>();
-            BaseSkill skill;
             SkillExtendedEntityProperties data = SkillExtendedEntityProperties.get(entity);
             data.catalog = type;
             while (prob >= RandUtils.nextFloat())
