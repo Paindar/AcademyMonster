26042b26ad4b4908cf26ea822886b96aca9e2d3c
Paindar <1944798085@qq.com>
2017-06-06 19:28:30 +0800

    【空间操控】添加方块保护的支持

--- src/main/java/cn/paindar/academymonster/ability/AILocManip.java
+++ src/main/java/cn/paindar/academymonster/ability/AILocManip.java
@@ -1,13 +1,17 @@
 package cn.paindar.academymonster.ability;
 
+import cn.academy.core.event.BlockDestroyEvent;
 import cn.lambdalib.util.generic.RandUtils;
 import cn.lambdalib.util.helper.BlockPos;
 import cn.lambdalib.util.mc.BlockSelectors;
 import cn.lambdalib.util.mc.WorldUtils;
+import cn.paindar.academymonster.config.AMConfig;
 import cn.paindar.academymonster.entity.EntityMagManipBlock;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.init.Blocks;
 import net.minecraft.util.Vec3;
 import net.minecraft.world.World;
+import net.minecraftforge.common.MinecraftForge;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -24,6 +28,8 @@ public class AILocManip extends BaseSkill
     Map<EntityMagManipBlock,Double> list=new HashMap<>();
     private int time=0;
     private float damage;
+    private double lastX,lastY,lastZ;
+    private boolean canDestroyBlock= AMConfig.getBoolean("am.skill.LocManip.destroyBlock",true);
     public AILocManip(EntityLivingBase speller,float exp)
     {
         super(speller,(int)lerpf(600,300,exp),exp,"teleporter.loc_manip");
@@ -34,15 +40,24 @@ public class AILocManip extends BaseSkill
         World world=speller.worldObj;
         if(world.isRemote || !canSpell())
             return; //w.getTileEntity(x,y,z)==null &&
-        List<BlockPos> list=WorldUtils.getBlocksWithin(speller, 7, (int)lerpf(25,100,getSkillExp()), BlockSelectors.filNormal,(world1, x, y, z, block) -> world1.getBlock(x,y,z).getBlockHardness(world1,x,y,z)>0);
         double range=3;
         isChanting=true;
         double rad=0,part=Math.acos(1.0-9.0/2/(range*range));
+        lastX=speller.posX;
+        lastY=speller.posY;
+        lastZ=speller.posZ;
+        List<BlockPos> list=WorldUtils.getBlocksWithin(speller, 7, (int)lerpf(25,100,getSkillExp()), BlockSelectors.filNormal,(world1, x, y, z, block) -> world1.getBlock(x,y,z).getBlockHardness(world1,x,y,z)>0);
         for(BlockPos pos:list)
         {
-            EntityMagManipBlock entity=  new EntityMagManipBlock(speller, 10,this);
-            entity.setBlock(pos.getBlock());
-            world.setBlockToAir(pos.x,pos.y, pos.z);
+            EntityMagManipBlock entity=  new EntityMagManipBlock(speller, damage,this);
+            if(!MinecraftForge.EVENT_BUS.post(new BlockDestroyEvent(world, pos.x,pos.y, pos.z)) &&canDestroyBlock)
+            {
+
+                entity.setBlock(pos.getBlock());
+                world.setBlockToAir(pos.x,pos.y, pos.z);
+            }
+            else
+                entity.setBlock(Blocks.ice);
             entity.setPosition(pos.x + .5, pos.y + .5, pos.z + .5);
             world.spawnEntityInWorld(entity);
             entity.setPlaceFromServer(false);
@@ -64,9 +79,21 @@ public class AILocManip extends BaseSkill
         if(!isChanting)
             return;
         time++;
-        if(time%3!=0 )
-            return;
-        List<EntityMagManipBlock> tmpList=new ArrayList<>();
+        double tx=0,ty=0,tz=0;
+        if(speller==null || speller.isDead)
+            time=250;
+        else
+        {
+            if (time % 3 != 0)
+                return;
+            tx = speller.posX - lastX;
+            ty = speller.posY - lastY;
+            tz = speller.posZ - lastZ;
+            lastX = speller.posX;
+            lastY = speller.posY;
+            lastZ = speller.posZ;
+        }
+        List<EntityMagManipBlock> tmpList = new ArrayList<>();
         for(Map.Entry<EntityMagManipBlock,Double> pair:list.entrySet())
         {
             EntityMagManipBlock entity=pair.getKey();
@@ -78,13 +105,17 @@ public class AILocManip extends BaseSkill
             if(time>=240)
             {
                 entity.setPlaceFromServer(true);
-                entity.stopMoveTo();
+                if(canDestroyBlock)
+                    entity.stopMoveTo();
+                else
+                    entity.setDead();
                 tmpList.add(entity);
                 continue;
             }
             entity.radium=(entity.radium+0.13)%(6.26);
             double rad=entity.radium;
-            Vec3 end=Vec3.createVectorHelper(speller.posX+Math.sin(rad)*pair.getValue(),speller.posY+2,speller.posZ+Math.cos(rad)*pair.getValue());
+            Vec3 end=Vec3.createVectorHelper(speller.posX+Math.sin(rad)*pair.getValue(),speller.posY+4,speller.posZ+Math.cos(rad)*pair.getValue());
+            entity.setPosition(entity.posX+tx,entity.posY+ty,entity.posZ+tz);
             entity.setMoveTo(end.xCoord,end.yCoord,end.zCoord);
         }
         for(EntityMagManipBlock entity:tmpList)
@@ -93,11 +124,5 @@ public class AILocManip extends BaseSkill
         }
     }
 
-    public void stop()
-    {
-        if(isChanting)
-        {
-            time=250;
-        }
-    }
+
 }
